33c675721214e1beaba9e2181676c48f9f8896d9
jubardt <dominiquejubard@gmail.com>
2020-10-13 10:52:14 +0200

    Q1
    question 1 correct

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
+import tp1.MotCles;
 
 public class Main {
 
     public static void main(String[] args) {
     	System.out.println("Test");
+    	MotCles m = new MotCles();
+    	System.out.println("Reponses question 1");
+    	System.out.println("====================");
+    	System.out.println("Test correct");
+    	System.out.println(m.keyword("Bonjour", "Je suis un test, Bonjour"));
+    	System.out.println("Test incorrect");
+    	System.out.println(m.keyword("Bonjour", "Je suis un test, Bonj"));
+    	
     }
 }
--- src/tp1/MotCles.java
+++ src/tp1/MotCles.java
@@ -10,25 +10,19 @@ public class MotCles {
     public boolean keyword(String word, String sentence) {
 
         boolean found = false;
-        int n = sentence.length();
-        int m = word.length();
-
-        for (int i = 0; i < n; i++) {
+        int n = sentence.length(); //Taille de la phrase
+        int m = word.length();	//Taille du mot 
+        for (int i = 0; i < n; i++) {	//On parcours 
             if(sentence.charAt(i) == word.charAt(0) && !found) {
                 found = true;
-
-                
                 for (int j = 1; j < m; j++) {
-                    if(i+j < m) {
-                        if (word.charAt(j) != sentence.charAt(i + j) && found) {
+                        if ((i+j >= n)||(word.charAt(j) != sentence.charAt(i + j) && found)) {
                             found = false;
-                        }
-                    }else{
-                        found = false;
-                    }
+                        }  
+                	
+                	}
                 }
-            }
-        }
+                }
         return found;
     }
 }
