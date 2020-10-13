5abe2f286320d0ec9cdd40bdd601959aff8ea4e8
Omar <40774438+omarmelade@users.noreply.github.com>
2020-10-13 10:08:41 +0200

    Update MotCles.java

--- src/tp1/MotCles.java
+++ src/tp1/MotCles.java
@@ -1,13 +1,34 @@
 package tp1;
 
 
-public class MotCles{
+public class MotCles {
 
     public MotCles() {
 
     }
 
-    public void keyword(){
+    public boolean keyword(String word, String sentence) {
 
+        boolean found = false;
+        int n = sentence.length();
+        int m = word.length();
+
+        for (int i = 0; i < n; i++) {
+            if(sentence.charAt(i) == word.charAt(0) && !found) {
+                found = true;
+
+                
+                for (int j = 1; j < m; j++) {
+                    if(i+j < m) {
+                        if (word.charAt(j) != sentence.charAt(i + j) && found) {
+                            found = false;
+                        }
+                    }else{
+                        found = false;
+                    }
+                }
+            }
+        }
+        return found;
     }
 }
