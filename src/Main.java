a06bc7b92b42287b0e9b12ea3531e00f2b9cceaf
o.khial <omarkhial74@gmail.com>
2020-10-13 10:05:25 +0200

    creating MotCles

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import tp1.MotCles;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        MotCles m = new MotCles();
+        boolean test = m.keyword("bonjour", "Je suis bonj , ca va ! bonjour ");
+        System.out.println(test);
     }
 }
