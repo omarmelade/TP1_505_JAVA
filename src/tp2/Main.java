08e8db4ed08c0939e2407900c457b9674d1b31de
jubardt <dominiquejubard@gmail.com>
2020-11-16 08:34:06 +0100

    re rand

--- src/tp2/Main.java
+++ src/tp2/Main.java
@@ -4,20 +4,28 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Main {
-	 public static void main(String[] args) {
-		 
-		 
-		 Reseau reseau = new Reseau(20);
-		 Colonie colonie = new Colonie(20);
-		 
-		 //Répartition des fourmis avec 1 ville = 1 fourmie
+	 
+	static Random rand =new Random();
+	 public static void repartitionAleatoire(Reseau reseau, Colonie colonie) {
 		 ArrayList<Ville> VillesAttribuables = reseau.getVilles();
-		 Random rand = new Random();
 		 for(int i = 0; i< colonie.getNbFourmi();i++) {
 			 int id = rand.nextInt(VillesAttribuables.size());
 			 colonie.getColonie().get(i).setVille(VillesAttribuables.get(id));
 			 VillesAttribuables.remove(id);
 		 }
+	 }
+	 public static void main(String[] args) {
+		 
+		 Reseau reseau = new Reseau(20);
+		 Colonie colonie = new Colonie(20);
+
+		 //Répartition des fourmis aléatoire avec 1 ville = 1 fourmie
+		 repartitionAleatoire(reseau,colonie);
 		 
 	 }
+	 
+	 
+	 
+	 
+
 }
